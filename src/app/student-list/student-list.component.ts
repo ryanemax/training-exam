79812e51be4461a9479f27281200dd98f36d6a2f
刘雨飏 <ryanemax@icloud.com>
2017-12-28 16:49:15 +0800

    feat: new module student

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,14 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 
+interface User{
+  id:number,
+  name:string,
+  github:string,
+  sex:string
+}
+
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
   styleUrls: ['./student-list.component.scss']
 })
 export class StudentListComponent implements OnInit {
-
-  constructor() { }
-
+  users:Array<User>;
+  constructor() { 
+    this.loadUsersData()
+  }
+  sortUsers(type){
+    // 参考MDN中的ES6，Array语法
+    // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
+    console.log("sortUsers Works!")
+  }
+  loadUsersData(){
+    this.users = [
+      {id:1,name:"Ryane",github:"ryanemax",sex:"male"},
+      {id:2,name:"Liming",github:"liming",sex:"male"},
+      {id:3,name:"Xiaohong",github:"xiaohong",sex:"female"}
+    ]
+  }
+  addNewUser(){
+    let newUser:User = {
+      id:Number(Math.random()*1000).toFixed(0),
+      name:"Jack",
+      github:"Jack",
+      sex:"male"
+    }
+    this.users.push(newUser);
+  }
+  deleteUserByID(id){
+    this.users.forEach((user,index,arr)=>{
+      if(user.id==id){
+        arr.splice(index,1);
+      }
+    })
+  }
   ngOnInit() {
   }
 
