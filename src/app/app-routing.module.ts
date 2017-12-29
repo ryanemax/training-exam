a92ee831865d2b86997ae6ae038e631be4bac854
suchao117 <suchao117@icloud.com>
2017-12-29 15:18:50 +0800

    feat: add supplier module & component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'bigdata',
     loadChildren: './bigdata/bigdata.module#BigdataModule',
+  },
+  {
+    path: 'supplier',
+    loadChildren: './supplier/supplier.module#SupplierModule',
   }
 ];
 
