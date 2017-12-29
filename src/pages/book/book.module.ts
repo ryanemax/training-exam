258dbd746e5879638c4d055b5fadae9c07e2d47f
niuhongjian <499326356@qq.com>
2017-12-29 20:30:46 +0800

    feat:book module

--- src/pages/book/book.module.ts
+++ src/pages/book/book.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { Routes, RouterModule } from '@angular/router';
 import { BookHomeComponent } from './book-home/book-home.component';
+import { BookItemComponent } from './book-item/book-item.component';
+import { DisableDirective } from './sharing/diable.directive';
 
 @NgModule({
   imports: [
@@ -11,6 +13,8 @@ import { BookHomeComponent } from './book-home/book-home.component';
       { path: '', component: BookHomeComponent, pathMatch: 'full' },
     ])
   ],
-  declarations: [BookHomeComponent]
+  declarations: [BookHomeComponent,
+    BookItemComponent,
+    DisableDirective]
 })
 export class BookModule { }
