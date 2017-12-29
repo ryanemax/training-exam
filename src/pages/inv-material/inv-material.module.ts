ca9d83c6c767ffa16ba3522160147a34551a5dc9
Li, Jiang <jiang.li@accenture.com>
2017-12-29 19:27:58 +0800

    feat: add new  inv-material

--- src/pages/inv-material/inv-material.module.ts
+++ src/pages/inv-material/inv-material.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { InvHomeComponent } from './inv-home/inv-home.component';
+import { InvItemComponent } from './inv-item/inv-item.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path: '', component: InvHomeComponent, pathMatch: 'full' },
+      { path: '', component: InvHomeComponent, pathMatch: 'full' }
     ])
   ],
-  declarations: [InvHomeComponent]
+  declarations: [InvHomeComponent,InvItemComponent]
 })
 export class InvMaterialModule { }
