8da11c986007e2f9128454812885d4d736341de8
suchao117 <suchao117@icloud.com>
2017-12-29 15:59:26 +0800

    fix: 1

--- src/pages/supplier/supplier.module.ts
+++ src/pages/supplier/supplier.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { SupplierListComponent } from '../supplier-list/supplier-list.component';
+import { SupplierListComponent } from './supplier-list/supplier-list.component';
 
 
 @NgModule({
