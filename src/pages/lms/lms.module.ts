a3f8b3bda67b6ab7c251f8656a79c9983841cda7
Liu <chang.h.liu@accenture.com>
2017-12-29 20:14:26 +0800

    merge

--- src/pages/lms/lms.module.ts
+++ src/pages/lms/lms.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { MatTabsModule } from '@angular/material/tabs';
+
 import { LmsHomeComponent } from './lms-home/lms-home.component';
 import { LmsStudentComponent } from './lms-student/lms-student.component';
 import { LmsClassComponent } from './lms-class/lms-class.component';
@@ -16,7 +16,7 @@ import { LmsScoreComponent } from './lms-score/lms-score.component';
       { path: 'class', component: LmsClassComponent, pathMatch: 'full' },
       { path: 'score', component: LmsScoreComponent, pathMatch: 'full' }
     ]),
-    MatTabsModule
+    
   ],
   declarations: [LmsHomeComponent, LmsStudentComponent, LmsClassComponent, LmsScoreComponent]
 })
