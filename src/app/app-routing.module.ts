7524dd8b55155b49606b5919c7a3c808f7256d17
niuhongjian <499326356@qq.com>
2017-12-29 15:47:22 +0800

    feat:book module add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'bigdata',
     loadChildren: './bigdata/bigdata.module#BigdataModule',
+  },
+  {
+    path: 'book',
+    loadChildren: '../pages/book/book.module#BookModule',
   }
 ];
 
