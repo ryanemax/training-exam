9f3f5493f08518d9886baf9593e0cc7b2dddcaf0
Li, Jiang <jiang.li@accenture.com>
2017-12-30 16:26:20 +0800

    feat: add dynamic data for inv-material

--- src/pages/inv-material/inv-home/inv-home.component.ts
+++ src/pages/inv-material/inv-home/inv-home.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptionsArgs } from '@angular/http';
 interface Item {
-  id: number,
   code: string,
   uom: string,
   description: string,
-  count: number
+  count: number,
+  objectId?:string;
+  updatedAt?:string;
+  createdAt?:string;
 }
 
 @Component({
@@ -15,30 +18,42 @@ interface Item {
 
 export class InvHomeComponent implements OnInit {
   items: Array<Item>;
-  constructor() {
+  constructor(private http:Http) {
     this.loadItemsData();
   }
 
   ngOnInit() {
   }
-  loadItemsData() {
-    this.items = [
-      {id: 1, code:"XX001",uom:"Each",description:"ring",count:1000},
-      {id: 2, code:"XX002",uom:"Each",description:"shell",count:3000},
-      {id: 3, code:"XX003",uom:"Each",description:"bolt and nut",count:66},
-      {id: 4, code:"XX004",uom:"Each",description:"block hoop",count:780},
-      {id: 5, code:"XX005",uom:"Each",description:"nameplate",count:34}
-    ];
+   loadItemsData() {
+  //   this.items = [
+  //     {id: 1, code:"XX001",uom:"Each",description:"ring",count:1000},
+  //     {id: 2, code:"XX002",uom:"Each",description:"shell",count:3000},
+  //     {id: 3, code:"XX003",uom:"Each",description:"bolt and nut",count:66},
+  //     {id: 4, code:"XX004",uom:"Each",description:"block hoop",count:780},
+  //     {id: 5, code:"XX005",uom:"Each",description:"nameplate",count:34}
+  //   ];
+  let url = "http://47.92.145.25:80/parse"+"/classes/InvItems";
+  let headers:Headers = new Headers();
+  headers.append("Content-Type","application/json");
+  headers.append("X-Parse-Application-Id","dev");
+  headers.append("X-Parse-Master-Key","angulardev");
+
+  let options ={
+    headers:headers
+  };
+  this.http.get(url,options).subscribe(data=>{
+    this.items = data.json().results;
+  });
   }
   sortItems(type) {
     // 参考MDN中的ES6，Array语法
     // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array
     if (type == 'asc') {
       this.items.sort(function (a, b) {
-        if (a.id > b.id) {
+        if (a.count > b.count) {
           return 1;
         }
-        if (a.id < b.id) {
+        if (a.count < b.count) {
           return -1;
         }
         return 0;
@@ -47,10 +62,10 @@ export class InvHomeComponent implements OnInit {
 
     if (type == 'desc') {
       this.items.sort(function (a, b) {
-        if (a.id > b.id) {
+        if (a.count > b.count) {
           return -1;
         }
-        if (a.id < b.id) {
+        if (a.count < b.count) {
           return 1;
         }
         return 0;
@@ -65,23 +80,51 @@ export class InvHomeComponent implements OnInit {
     console.log("sortUsers Works!");
   }
   addNewItem() {
-    let uuid = Number(Math.random() * 1000).toFixed(0);
+    // let newItem: Item = {
+    //   id: Number(uuid),
+    //   code: "XXFS01",
+    //   uom: "Meter",
+    //   description: "test",
+    //   count: 666
+    // }
+    let url = "http://47.92.145.25:80/parse"+"/classes/User12";
+    let headers:Headers = new Headers();
+    headers.append("Content-Type","application/json");
+    headers.append("X-Parse-Application-Id","dev");
+    headers.append("X-Parse-Master-Key","angulardev");
+    let options ={
+      headers:headers
+    };
     let newItem: Item = {
-      id: Number(uuid),
       code: "XXFS01",
-      uom: "Meter",
-      description: "test",
-      count: 666
-    }
+       uom: "Meter",
+       description: "test",
+       count: 666
+    };
+    this.http.post(url,newItem,options).subscribe(data=>{
+      this.loadItemsData();
+    });
     this.items.push(newItem);
   }
 
   deleteItemByID(id) {
-    this.items.forEach((item, index, arr)=> {
-      if (item.id == id) {
-        arr.splice(index, 1);
-      }
-    })
+    // this.items.forEach((item, index, arr)=> {
+    //   if (item.id == id) {
+    //     arr.splice(index, 1);
+    //   }
+    // })
+    let url = "http://47.92.145.25:80/parse"+"/classes/User12"+"/"+id;
+    let headers:Headers = new Headers();
+    headers.append("Content-Type","application/json");
+    headers.append("X-Parse-Application-Id","dev");
+    headers.append("X-Parse-Master-Key","angulardev");
+    let options ={
+      headers:headers
+    };
+
+    this.http.delete(url,options).subscribe(data=>{
+      this.loadItemsData();
+    });
   }
 
 }
