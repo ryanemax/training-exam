02a33c23b0767a2e84ccd9c09e5945912beacbde
Liu <chang.h.liu@accenture.com>
2017-12-30 09:41:23 +0800

    merge

--- src/pages/flower/flower-home/flower-home.component.ts
+++ src/pages/flower/flower-home/flower-home.component.ts
@@ -48,7 +48,7 @@ export class FlowerHomeComponent implements OnInit {
   }
   deleteFlowerByID(id){
     this.flowers.forEach((user,index,arr)=>{
-      if(user.id==id){
+      if(flower.id==id){
         arr.splice(index,1);
       }
     })
