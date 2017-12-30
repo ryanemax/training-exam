417af732eb45fd75a4a1452adc9b2bd9c5b19163
jiankai.feng <jiankai.feng@accenture.com>
2017-12-30 16:35:14 +0800

    fix: compile error

--- src/pages/kqgl/kqgl.module.ts
+++ src/pages/kqgl/kqgl.module.ts
@@ -4,10 +4,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { KqglHomeComponent } from './kqgl-home/kqgl-home.component';
 import { Button1Directive } from './sharing/button1.directive';
 import { ToDateYmd } from './sharing/datetr.pipe';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material';
+import { MatOptionModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material';
+
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   imports: [
     CommonModule,
-
+    MatTabsModule,
+    MatButtonModule,
+    MatFormFieldModule,MatInputModule,
+    FormsModule,MatIconModule,MatMenuModule,
+    MatSelectModule,MatOptionModule,
     RouterModule.forChild([
       { path: '', component: KqglHomeComponent, pathMatch: 'full' },
     ])
@@ -16,6 +32,7 @@ import { ToDateYmd } from './sharing/datetr.pipe';
     KqglHomeComponent,
     Button1Directive,
     ToDateYmd
+    
   ]
 })
 export class KqglModule { }
