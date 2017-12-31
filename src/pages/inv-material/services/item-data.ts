beee2be1e59e764674038561b51446ee9018b76e
Li, Jiang <jiang.li@accenture.com>
2017-12-31 22:45:11 +0800

    feat: inv-material add charts

--- src/pages/inv-material/services/item-data.ts
+++ src/pages/inv-material/services/item-data.ts
@@ -6,6 +6,7 @@ import {Item} from '../models/item-model';
 // import { Observable } from '../../../node_modules/_rxjs@5.5.2@rxjs/Observable';
 
 
+
   interface ParseResponse {
     results: Item[];
   }
@@ -13,18 +14,20 @@ import {Item} from '../models/item-model';
 @Injectable()
 export class ItemService{
     items : Item[] ;
-    
-    
+    headers: HttpHeaders;
+    url : any;
     constructor(private http:HttpClient){
+    this.headers = new HttpHeaders();
+    this.headers = this.headers.set("Content-Type","application/json").set("X-Parse-Application-Id","dev").set("X-Parse-Master-Key","angulardev");
+    this.url = "http://47.92.145.25:80/parse"+"/classes/InvItems";
     }
+    LmsStudentSerice
     loadItemsData() : Item[] {
-      let url = "http://47.92.145.25:80/parse"+"/classes/InvItems";
-      let headers:HttpHeaders = new HttpHeaders();
-      headers = headers.set("Content-Type","application/json").set("X-Parse-Application-Id","dev").set("X-Parse-Master-Key","angulardev");
+      
       let options:any ={
-        headers:headers
+        headers:this.headers
       };
-      this.http.get<ParseResponse>(url,options).subscribe(data=>{
+      this.http.get<ParseResponse>(this.url,options).subscribe(data=>{
         this.items = data['results'];
       });
       return this.items;
@@ -71,17 +74,19 @@ export class ItemService{
         };
         if(!item.objectId){
           console.log(item);
-          this.http.post(url,item,options).subscribe(data=>{
-            this.loadItemsData();
-          });
+          // this.http.post(url,item,options).subscribe(data=>{
+          //   this.loadItemsData();
+          // });
+          return this.http.post(url,item,options);
         } else{
           url = "http://47.92.145.25:80/parse"+"/classes/InvItems/"+item.objectId;
           delete item["objectId"];
           delete item["createdAt"];
           delete item["updatedAt"];
-          this.http.put(url,item,options).subscribe(data=>{
-            this.loadItemsData();
-          });
+          // this.http.put(url,item,options).subscribe(data=>{
+          //   this.loadItemsData();
+          // });
+          return this.http.put(url,item,options);
         }
       }
       deleteItemByID(id) {
@@ -91,8 +96,16 @@ export class ItemService{
         let options ={
           headers:headers
         };
-        this.http.delete(url,options).subscribe(data=>{
-          this.loadItemsData();
-        });
+        return this.http.delete(url,options);
+        // this.http.delete(url,options).subscribe(data=>{
+        //   this.loadItemsData();
+        // });
+      }
+      getItems(): Observable<Object> {
+        let options = {
+          headers: this.headers
+        };
+      
+        return this.http.get(this.url, options);
       }
 }
