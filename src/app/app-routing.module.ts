ac0effda23e38ca58d0f26f9730ae75de203cfff
Liu <jiawen.a.liu@accenture.com>
2017-12-31 16:43:16 +0800

    merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -96,6 +96,10 @@ const routes: Routes = [
   {
     path: 'clothes1',
     loadChildren: '../pages/clothes1/clothes1.module#Clothes1Module',
+  },
+  {
+    path: 'coffee-star1',
+    loadChildren: '../pages/coffee-star1/coffee-star1.module#CoffeeStar1Module',
   }
 
 ];
